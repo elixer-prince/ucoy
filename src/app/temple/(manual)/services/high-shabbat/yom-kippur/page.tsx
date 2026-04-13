f176f2445531461eeb44c772b0ea74f855c2b0ec
Daniel Leith <elixerprince@gmail.com>
2026-04-13 18:32:32 -0500

    feat(high-shabbat): cretae yom kippur and yom teruah pages

---
+++ src/app/temple/(manual)/services/high-shabbat/yom-kippur/page.tsx
@@ -0,0 +1,15 @@
+import TheShema from "@/app/temple/(manual)/_components/sections/TheShema";
+
+const Page = () => {
+  return (
+    <>
+      <header>
+        <h1>Yom Kippur</h1>
+      </header>
+
+      <TheShema />
+    </>
+  );
+};
+
+export default Page;
---
+++ src/app/temple/(manual)/services/high-shabbat/yom-teruah/page.tsx
@@ -0,0 +1,15 @@
+import TheShema from "@/app/temple/(manual)/_components/sections/TheShema";
+
+const Page = () => {
+  return (
+    <>
+      <header>
+        <h1>Yom Teruah</h1>
+      </header>
+
+      <TheShema />
+    </>
+  );
+};
+
+export default Page;
