19840dd11c90291bc88477d55fa37688994dd711
Daniel Leith <elixerprince@gmail.com>
2026-04-13 18:29:31 -0500

    feat(sections): create shema section

---
+++ src/app/temple/(manual)/_components/sections/TheShema.tsx
@@ -0,0 +1,126 @@
+import HebrewText from "../../rituals/home/HebrewText";
+
+const TheShema = () => {
+  return (
+    <section>
+      <header>
+        <h2 className="font-bold">The Shema</h2>
+
+        <p>
+          <HebrewText translation="Testing" transliterated>
+            Shema Yisrael, Yahweh Shaddai, Yahweh echad. Baruch shem kavod
+            malchuto l’olam va’ed
+          </HebrewText>
+        </p>
+      </header>
+
+      <section>
+        <h3 className="font-bold">
+          <HebrewText translation="Deuteronomy" transliterated>
+            Debarim
+          </HebrewText>{" "}
+          6:4-9
+        </h3>
+
+        <p>
+          “Hear, O Yisra’Yah: יהוה All-Mighty, יהוה is one! “And you shall love
+          יהוה your All-Mighty with all your heart, with all your being, and
+          with all your might. “And these Words which I am commanding you hayom{" "}
+          {"{today}"} shall be in your heart, And you shall impress them upon
+          your children, and shall speak of them when you sit in your house, and
+          when you walk by the way, and when you lie down, and when you rise up
+          and shall bind them as a sign on your hand, and they shall be as
+          frontlets between your eyes. “And you shall write them on the
+          doorposts of your house and on your gates.
+        </p>
+      </section>
+
+      <section>
+        <h3 className="font-bold">
+          <HebrewText translation="Deuteronomy" transliterated>
+            Debarim
+          </HebrewText>{" "}
+          11:13-21
+        </h3>
+
+        <p>
+          ‘And it shall be that if you diligently obey My commands which I
+          command you hayom {"{today}"}, to love יהוה your Ab-ba and to serve
+          Him with all your heart and with all your being, then I shall give you
+          the rain for your land in its season, the early rain and the latter
+          rain, and you shall gather in your grain, and your new wine, and your
+          oil.{" "}
+        </p>
+
+        <p>
+          ‘And I shall give grass in your fields for your livestock, and you
+          shall eat and be satisfied. ‘Guard yourselves, lest your heart be
+          deceived, and you turn aside and serve other mighty ones and bow down
+          to them. ‘Then the displeasure of יהוה shall burn against you, and He
+          shall shut up the shamayim {"{heaven}"}, and there be no rain, and the
+          lands not give its increase. And you shall perish quickly from the tov{" "}
+          {"{good}"} land which יהוה is giving you. ‘And you shall lay up these
+          words of Mine in your heart and in your being, and shall bind them as
+          a sign on your hand, and they shall be as frontlets between your
+          eyes.{" "}
+        </p>
+
+        <p>
+          ‘And you shall teach them to your children, speaking of them when you
+          sit in your house, and when you walk by the way, when you lie down,
+          and when you rise up. And shall write them on the doorposts of your
+          house and on your gates, So that your chai {"{life}"} and the Chai{" "}
+          {"{life}"} of your children are increased on the soil of which יהוה
+          swore to your fathers to give them, as the yomim {"{days}"} of the
+          shamayim {"{heavens}"} on the erets {"{earth}"}.
+        </p>
+      </section>
+
+      <section>
+        <h3 className="font-bold">
+          <HebrewText translation="Numbers" transliterated>
+            Bemidbar
+          </HebrewText>{" "}
+          15:37-41
+        </h3>
+
+        <p>
+          And יהוה spoke to Mosheh {"{Moses}"}, saying, “Speak to the children
+          of Yisra’Yah, and you shall say to them to make tzitzit
+          {"{fringes}"} on the corners of their garments throughout their
+          generations, and to put a blue cord in the tzitzit of the corners.
+        </p>
+
+        <p>
+          “And it shall be to you for a tzitzit, and you shall see it, and shall
+          remember all the commands of יהוה and shall do them, and not search
+          after your own heart and your own eyes after which you went whoring,
+          So that you remember, and shall do all My commands, and be set-apart
+          unto your Ab-ba.
+        </p>
+
+        <p>
+          “I am <HebrewText translation="Yahweh">יהוה</HebrewText> your{" "}
+          <HebrewText translation="Father" transliterated>
+            Abba
+          </HebrewText>
+          , who brought you out of the land of{" "}
+          <HebrewText translation="Egypt" transliterated>
+            Mitsrayim
+          </HebrewText>
+          , to be your{" "}
+          <HebrewText translation="Father" transliterated>
+            Abba
+          </HebrewText>
+          . I am <HebrewText translation="Yahweh">יהוה</HebrewText> your{" "}
+          <HebrewText translation="Father" transliterated>
+            Abba
+          </HebrewText>
+          .”
+        </p>
+      </section>
+    </section>
+  );
+};
+
+export default TheShema;
