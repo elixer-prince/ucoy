5bf28086d27dc8573de92c9947e469947229d8af
Daniel Leith <elixerprince@gmail.com>
2026-04-13 23:25:15 -0500

    feat: add shabbat morning page boilerplate

--- src/app/temple/(manual)/services/shabbat-morning/page.tsx
+++ src/app/temple/(manual)/services/shabbat-morning/page.tsx
@@ -1,5 +1,22 @@
-const ShabbatMorningService = () => {
-  return <></>;
+import TheShema from "@/app/temple/(manual)/_components/sections/TheShema";
+
+const Page = () => {
+  return (
+    <>
+      <header>
+        <h1 className="font=bold">Shabbat Morning Service</h1>
+
+        <div>
+          <p>Shabbat Shalom (Happy Sabbath) my brothers and sisters!</p>
+          <p>
+            Welcome to the Heykal (Temple) of Yahweh and the Shabbat Celebration
+          </p>
+        </div>
+      </header>
+
+      <TheShema />
+    </>
+  );
 };
 
-export default ShabbatMorningService;
+export default Page;
--- src/ui/components/Navbar/DesktopLinks.tsx
+++ src/ui/components/Navbar/DesktopLinks.tsx
@@ -1,3 +1,4 @@
+import { WEEKLY } from "@/lib/constants/rituals";
 import { HIGH_SHABBATOT } from "@/lib/constants/services";
 import Link from "next/link";
 
@@ -9,28 +10,66 @@ const DesktopLinks = () => {
       </li>
 
       <li>
-        <span className="font-bold">High Shabbat</span>
+        <details className="static open:rounded-full">
+          <summary>Services</summary>
 
-        <ul>
-          {HIGH_SHABBATOT.map(({ title, url }, index) => (
-            <li key={index}>
-              <Link href={url}>{title}</Link>
+          <ul className="absolute top-15 w-40 bg-black">
+            <li>
+              <ul>
+                <li>
+                  <Link
+                    className="block px-4 py-2 hover:bg-white/25"
+                    href="/temple/services/shabbat-morning"
+                  >
+                    Shabbat Morning Service
+                  </Link>
+                </li>
+
+                <details>
+                  <summary>High Shabbat</summary>
+
+                  {HIGH_SHABBATOT.map(({ title, url }, index) => (
+                    <li key={index}>
+                      <Link
+                        className="block px-4 py-2 hover:bg-white/25"
+                        href={url}
+                      >
+                        {title}
+                      </Link>
+                    </li>
+                  ))}
+                </details>
+              </ul>
             </li>
-          ))}
-        </ul>
+          </ul>
+        </details>
+      </li>
+
+      <li>
+        <details className="static open:rounded-full">
+          <summary>Rituals</summary>
+
+          <ul className="absolute top-15 w-40 bg-black">
+            {WEEKLY.map(({ title, url }, index) => (
+              <li key={index}>
+                <Link className="block px-4 py-2 hover:bg-white/25" href={url}>
+                  {title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </details>
       </li>
 
       <li>
         <Link href="/temple/treasury">Treasury</Link>
       </li>
       <li>
-        <Link href="/temple/rituals/home/friday-evening">
-          Friday Evening Home Ritual
-        </Link>
+        <Link href="/temple/attendance">Attendance</Link>
       </li>
       <li>
-        <Link href="/temple/services/shabbat-morning">
-          Shabbat Morning Service
+        <Link href="/temple/rituals/home/friday-evening">
+          Friday Evening Home Ritual
         </Link>
       </li>
     </ul>
