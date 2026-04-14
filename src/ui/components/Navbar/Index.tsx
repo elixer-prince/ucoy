71a094aafab6c5bea320dcc0a7a75f6c00931532
Daniel Leith <elixerprince@gmail.com>
2026-04-14 11:28:48 -0500

    fix(navbar): remove nested button to fix clerk errors

--- src/ui/components/Navbar/Index.tsx
+++ src/ui/components/Navbar/Index.tsx
@@ -16,11 +16,7 @@ const Navbar = () => {
 
         <Show when="signed-out">
           <SignInButton />
-          <SignUpButton>
-            <button className="h-10 cursor-pointer rounded-full bg-purple-700 px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
-              Sign Up
-            </button>
-          </SignUpButton>
+          <SignUpButton />
         </Show>
 
         <Show when="signed-in">
